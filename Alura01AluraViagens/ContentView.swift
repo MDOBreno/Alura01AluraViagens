e744c1efc67d2944db3900faf91d8acd4d7d091b
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-09-18 00:57:35 -0300

    - Aula 01 Concluida

--- Alura01AluraViagens/ContentView.swift
+++ Alura01AluraViagens/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text("Primeira aula de SwiftUi")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+/*struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
+}*/
